11228b93eacc4a39a3dc510f7a5dc18b8b0a0628
czg <czgsci@163.com>
2023-08-19 17:40:08 +0800

    update 'initial-reverse'

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -4,11 +4,16 @@
 #include <sys/stat.h> // fstat
 #include <sys/types.h>
 
+/*
+1. no need to use do{}while(0) after comparing the assembled codes.
+2. Assumptions 7
+*/
 #define handle_error(msg)                                                      \
-  do {                                                                         \
+    {\
     fprintf(stderr, msg);                                                      \
     exit(EXIT_FAILURE);                                                        \
-  } while (0)
+    }                   
+  // } while (0)
 
 typedef struct linkedList {
   char *line;
@@ -26,7 +31,7 @@ int main(int argc, char *argv[]) {
   }
 
   if (argc == 3) {
-    if ((in = fopen(argv[1], "r")) == NULL) {
+    if ((in = fopen(argv[1], "r")) == NULL) { // Assumptions 4
       fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
       exit(EXIT_FAILURE);
     }
@@ -38,17 +43,20 @@ int main(int argc, char *argv[]) {
     struct stat sb1, sb2;
     if (fstat(fileno(in), &sb1) == -1 || fstat(fileno(out), &sb2) == -1)
       handle_error("reverse: fstat error\n");
-    if (sb1.st_ino == sb2.st_ino)
-      handle_error("reverse: input and output file must differ\n");
+    if (sb1.st_ino == sb2.st_ino) // see https://man7.org/linux/man-pages/man2/lstat.2.html
+      handle_error("reverse: input and output file must differ\n"); // Assumptions 1
   } else if (argc > 3)
-    handle_error("usage: reverse <input> <output>\n");
+    handle_error("usage: reverse <input> <output>\n"); // Assumptions 6
 
   LinkedList *head = NULL;
   char *line = NULL;
   size_t len = 0;
   // store lines to list in reverse order
-  while (getline(&line, &len, in) != -1) {
-    LinkedList *node = malloc(sizeof(LinkedList));
+  while (getline(&line, &len, in) != -1) { // Assumptions 2,3
+    /*
+    from README: The routine malloc() is useful for memory allocation. Perhaps for adding elements to a list?
+    */
+    LinkedList *node = malloc(sizeof(LinkedList)); // Assumptions 5
     if (node == NULL) {
       free(line);
       handle_error("reverse: malloc failed\n");
@@ -61,6 +69,9 @@ int main(int argc, char *argv[]) {
     head = node;
   }
 
+  /*
+  from README: Then, we printed out the list in reverse order. Then we made sure to handle error cases. And so forth...
+  */
   // print reversed lines
   while (head != NULL) {
     LinkedList *temp = head;
@@ -73,5 +84,6 @@ int main(int argc, char *argv[]) {
   free(line);
   fclose(in);
   fclose(out);
-  return 0;
+  // return 0;
+  exit(1);
 }
